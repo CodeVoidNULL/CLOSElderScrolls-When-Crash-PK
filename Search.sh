b71985aba2ff048968ba4d417b03bba345581246
Austin <69089015+TheScriptingDev@users.noreply.github.com>
2020-08-23 20:05:20 -0500

    Update Search.sh

--- Search.sh
+++ Search.sh
@@ -3,7 +3,6 @@ ps -x > OutputFile.txt
 clear
 grep -i 'The Elder Scrolls V Skyrim Legendary' OutputFile.txt > NarrowedDownlist.txt
 sed 's/\??.*/??/' NarrowedDownlist.txt > pidsByList.txt
-
 while IFS= read -r line; do
    kill $line
 done < pidsByList.txt
